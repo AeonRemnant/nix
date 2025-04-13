5b239aaba61329a711ebef7454ddd9ba5e9b94f1
AeonRemnant <aeonremnant@github.com>
2025-04-13 16:57:20 +1000

    Fixed stupid error.

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -27,18 +27,20 @@
   # === Services ===
   services.mako.enable = true; 
 
-  # === Nvidia Flags ===
-  home.sessionVariables = {
-  LIBVA_DRIVER_NAME = "nvidia";
-  __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  GBM_BACKEND = "nvidia-drm";
-  ELECTRON_OZONE_PLATFORM_HINT = "auto";
-  QT_QPA_PLATFORM = "wayland";
-  QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-  QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-  QT_QPA_PLATFORMTHEME = "qt6ct";
-  };
-
   # === Environment ===
-  HYPRSPACE = "${pkgs.hyprlandPlugins.hyprspace}/lib/hyprland/plugins/hyprspace.so";
+  home.sessionVariables = {
+    
+    # Nvidia flags
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_QPA_PLATFORMTHEME = "qt6ct";
+
+    # Hyprland plugins
+    HYPRSPACE = "${pkgs.hyprlandPlugins.hyprspace}/lib/hyprland/plugins/hyprspace.so";
+  };
 }
