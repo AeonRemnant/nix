049c7500de8a0bdc4700e364095a68764c06f45d
AeonRemnant <aeonremnant@github.com>
2025-04-17 20:37:47 +1000

    Potential fix for loading Hyprspace correctly using extraConfig.

--- modules/system/hyprland.nix
+++ modules/system/hyprland.nix
@@ -5,5 +5,8 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+    extraConfig = ''
+      plugin = ${pkgs.hyprlandPlugins.hyprspace}/lib/hyprland/plugins/hyprspace.so
+    '';
   };
 }
