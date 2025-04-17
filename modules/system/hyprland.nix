c7780d24e639f83803868770be6c54943273fe3c
AeonRemnant <aeonremnant@github.com>
2025-04-17 20:10:56 +1000

    FIx

--- modules/system/hyprland.nix
+++ modules/system/hyprland.nix
@@ -5,10 +5,5 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    extraConfig = ''
-      # Load hyprspace plugin
-      plugin = ${pkgs.hyprlandPlugins.hyprspace}/lib/hyprland/plugins/hyprspace.so
-      # Add any other essential plugins here
-    '';
   };
 }
