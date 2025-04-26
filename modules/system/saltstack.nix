c86319ae3136803b315c1dcaf9ad31e043cd3a87
AeonRemnant <aeonremnant@github.com>
2025-04-27 06:35:43 +1000

    Updated Salt.

--- modules/system/saltstack.nix
+++ modules/system/saltstack.nix
@@ -7,6 +7,11 @@
   ];
   services.salt.master = {
     enable = true;
+    config = ''
+      cloud_providers_dir: /home/aeon/git/aeonremnant/salt/cloud.providers.d
+      cloud_profiles_dir: /home/aeon/git/aeonremnant/salt/cloud.profiles.d
+      cloud_maps: /home/aeon/git/aeonremnant/salt/cloud.map.d
+    '';
   };
   networking.firewall.allowedTCPPorts = [ 4505 4506 ];
 }
