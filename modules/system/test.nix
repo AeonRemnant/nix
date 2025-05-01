f968adbc1a532586e7de8285b523fd9902eb6009
AeonRemnant <aeonremnant@github.com>
2025-05-01 23:10:13 +1000

    Removed mesa git, it was broken.

--- modules/system/test.nix
+++
@@ -1,5 +0,0 @@
-{ config, pkgs, chaotic, lib, ... }:
-
-{
-    chaotic.mesa-git.enable
-}
--- nixos/forge/configuration.nix
+++ nixos/forge/configuration.nix
@@ -13,7 +13,6 @@
     ../../modules/system/gaming.nix
     ../../modules/system/ports.nix
     ../../modules/system/kernel.nix
-    ../../modules/system/test.nix
   ];
 
   # === System Config ===
